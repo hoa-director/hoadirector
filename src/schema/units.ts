d2ef9d46a913d2c1d73fa3da76a70261f9c3a39a
dexter-stpierre <dexter.stpierre.dev@gmail.com>
2018-08-01 00:36:06 -0500

    added rules schema

---
+++ src/schema/units.ts
@@ -0,0 +1,60 @@
+import * as Sequelize from 'sequelize';
+import connection from '../config/database';
+
+export class Rules extends Sequelize.Model {
+    id: number;
+    userId: number;
+    associationId: number;
+    addressLineOne: string;
+    addressLineTwo: string;
+    city: string;
+    state: string;
+    zip: number;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date;
+}
+
+Rules.init(
+    {
+        id: {
+            type: Sequelize.INTEGER({ length: 10}),
+            primaryKey: true,
+            unique: true,
+            autoIncrement: true,
+            field: 'id',
+        },
+        userId: {
+            type: Sequelize.INTEGER({ length: 10 }),
+            field: 'user_id',
+        },
+        associationId: {
+            type: Sequelize.INTEGER({ length: 10 }),
+            field: 'association_id',
+        },
+        addressLineOne: {
+            type: Sequelize.STRING(100),
+            field: 'address_line_one',
+        },
+        addressLineTwo: {
+            type: Sequelize.STRING(100),
+            field: 'address_line_two',
+        },
+        city: {
+            type: Sequelize.STRING(60),
+            field: 'city',
+        },
+        state: {
+            type: Sequelize.STRING(45),
+            field: 'state',
+        },
+        zip: {
+            type: Sequelize.INTEGER({ length: 10 }),
+            field: 'zip',
+        },
+    },
+    { sequelize: connection }
+);
+
+export const RulesSchema = Rules;
+export default RulesSchema;
