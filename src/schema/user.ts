af11e19aec315cec8826bab2b0238bdcbfcdfee5
dexter-stpierre <dexter.stpierre.dev@gmail.com>
2018-08-01 00:09:56 -0500

    completed user schema

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -9,6 +9,11 @@ export class User extends Sequelize.Model {
     id: number;
     email: string;
     password: string;
+    number: number;
+    role: number;
+    firstName: string;
+    lastName: string;
+    fullName: string;
     createdAt: Date;
     updatedAt: Date;
     deletedAt: Date;
@@ -50,15 +55,35 @@ User.init(
                 isEmail: true
             },
             unique: true,
-            field: 'email'
+            field: 'email',
         },
         password: {
             type: Sequelize.STRING(45),
             validate: {
                 max: 45
             },
-            field: 'password'
+            field: 'password',
         },
+        number: {
+            type: Sequelize.INTEGER({ length: 11 }),
+            field: 'number',
+        },
+        role: {
+            type: Sequelize.INTEGER({ length: 2 }),
+            field: 'role',
+        },
+        firstName: {
+            type: Sequelize.STRING(45),
+            field: 'first_name',
+        },
+        lastName: {
+            type: Sequelize.STRING(45),
+            field: 'last_name',
+        },
+        fullName: {
+            type: Sequelize.STRING(91),
+            field: 'full_name',
+        }
     },
     { sequelize: connection }
 );
@@ -66,50 +91,7 @@ User.init(
 User.beforeCreate((user, options) => {
     const encryptedPassword = User.encryptPassword(user.password);
     user.password = encryptedPassword;
-})
-// export interface UserAddModel {
-//     email: string;
-//     password: string;
-// }
-
-// export interface UserModel extends Sequelize.Model<UserModel, UserAddModel> {
-//     id: number;
-//     email: string;
-//     password: string;
-//     createdAt: string;
-//     updatedAt: string;
-// }
-
-// export interface UserViewModel {
-//     id: number;
-//     email: string;
-// }
-
-// export const User = connection.define<UserModel, UserAddModel>('user', {
-    // id: {
-    //     type: Sequelize.INTEGER(10),
-    //     primaryKey: true,
-    //     unique: true,
-    //     autoIncrement: true,
-    //     field: 'id',
-    // },
-    // email: {
-    //     type: Sequelize.STRING(100),
-    //     validate: {
-    //         max: 100,
-    //         isEmail: true
-    //     },
-    //     unique: true,
-    //     field: 'email'
-    // },
-    // password: {
-    //     type: Sequelize.STRING(45),
-    //     validate: {
-    //         max: 45
-    //     },
-    //     field: 'password'
-    // },
-// });
+});
 
 export const userSchema = User;
 export default User;
