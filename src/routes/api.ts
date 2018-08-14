7daf54db5f07d7fb29ee0c1631eb4c5e2971335a
dexter-stpierre <dexter.stpierre.dev@gmail.com>
2018-08-13 20:55:04 -0500

    directory route working

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import * as path from 'path';
 import * as fs from 'fs';
 
-import { Document, RuleList } from '../schema/schemas';
+import { Association, Document, RuleList } from '../schema/schemas';
 
 export class ApiRouter {
   router: Router;
@@ -15,6 +15,7 @@ export class ApiRouter {
   init() {
     this.router.get('/documents', this.getDocumnets);
     this.router.get('/documents/:id', this.viewDocument);
+    this.router.get('/directory', this.getDirectory);
     this.router.get('/rules', this.getRules);
     this.router.get('/', (req: Request, res: Response, next: NextFunction) => {
       res.sendStatus(200);
@@ -29,6 +30,15 @@ export class ApiRouter {
     const associationId = req.session.associationId;
     Document.getDocumentsByAssociation(associationId).then(documents => {
       res.send(documents);
+    }).catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+  }
+  private getDirectory = (req: Request, res: Response, next: NextFunction) => {
+    const associationId = req.session.associationId;
+    Association.getDirectoryByAssociationId(associationId).then(association => {
+      res.send(association);
     }).catch(error => {
       console.log(error);
       res.sendStatus(500);
--- src/schema/association.ts
+++ src/schema/association.ts
@@ -1,11 +1,41 @@
 import * as Sequelize from 'sequelize';
 
+import { Unit } from './unit';
+import { User } from './user';
+
 export class Association extends Sequelize.Model {
     id: number;
     name: string;
     createdAt: Date;
     updatedAt: Date;
     deletedAt: Date;
+
+    public static getDirectoryByAssociationId(associationId: number) {
+        return new Promise((resolve, reject) => {
+            Association.find({
+                where: {id: associationId},
+                attributes: ['name'],
+                include: [
+                    {
+                        model: Unit, 
+                        as: 'units',
+                        attributes: ['addressLineOne', 'addressLineTwo', 'city', 'state', 'zip'],
+                        include: [
+                            {
+                                model: User,
+                                as: 'user',
+                                attributes: ['fullName', 'email', 'number']
+                            }
+                        ]
+                    }
+                ]
+            }).then(association => {
+                resolve(association);
+            }).catch(error => {
+                reject(error);
+            });
+        })
+    }
 
     public static init(sequelize) {
         super.init(
