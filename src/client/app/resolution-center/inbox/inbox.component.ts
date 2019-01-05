c930b0918a149d6fbb91fdc15ffc68a994d7ebef
dexter St Pierre <dexter.stpierre.dev@gmail.com>
2019-01-05 08:02:40 -0600

    Outbox/inbox uses address
    - Outbox and inbox now uses address rather than name per Gregs request

--- src/client/app/resolution-center/inbox/inbox.component.ts
+++ src/client/app/resolution-center/inbox/inbox.component.ts
@@ -4,35 +4,33 @@ import { ResolutionCenterService } from '../resolution-center.service';
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
-  styleUrls: ['./inbox.component.css']
+  styleUrls: ['./inbox.component.css'],
 })
 export class InboxComponent implements OnInit {
-
   public objections: any[] = [];
 
   public currentObjection;
 
-  constructor(
-    private resolutionCenterService: ResolutionCenterService,
-  ) { };
+  constructor(private resolutionCenterService: ResolutionCenterService) {}
 
   ngOnInit() {
-    this.resolutionCenterService.getInbox().subscribe(response => {
+    this.resolutionCenterService.getInbox().subscribe((response) => {
       this.objections = response.objections;
     });
-  };
+  }
 
   selectObjection(objection) {
     this.currentObjection = objection;
   }
 
   vote(approved) {
-    this.resolutionCenterService.submitVote({
-      approved, 
-      objectionId: this.currentObjection.id
-    }).subscribe(response => {
-      console.log(response);
-    });
-  };
-
-};
+    this.resolutionCenterService
+      .submitVote({
+        approved,
+        objectionId: this.currentObjection.id,
+      })
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+}
