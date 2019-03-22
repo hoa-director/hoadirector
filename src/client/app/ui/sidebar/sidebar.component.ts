df1b9860c8cc5446621b7c8ecebdb2ad22d6de4f
Dexter St-Pierre <dexter.stpierre.dev@gmail.com>
2019-03-21 21:22:39 -0500

    Updated sidebar to hide features that aren't complete

--- src/client/app/ui/sidebar/sidebar.component.ts
+++ src/client/app/ui/sidebar/sidebar.component.ts
@@ -7,50 +7,59 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit, OnDestroy {
-
-  userIsLoggedIn: boolean = false;
+  userIsLoggedIn = false;
   subscriptions = [];
 
-  links: Array<{ path: string; label: string }> = [
+  links: Array<{ path: string; label: string; active: boolean }> = [
     {
       path: 'directory',
       label: 'Neighborhood Directory',
+      active: true,
     },
     {
       path: 'rules',
       label: 'Rules & Regulations',
+      active: true,
     },
     {
       path: 'resolution-center',
       label: 'Resolution Center',
+      active: true,
     },
     {
       path: 'financials',
       label: 'Financials',
+      active: false,
     },
     {
       path: 'documents',
       label: 'Association Documents',
+      active: true,
     },
     {
       path: 'exterior',
       label: 'Exterior Maintenance',
+      active: false,
     },
     {
       path: 'interior',
       label: 'Interior Mainenance',
+      active: false,
     },
     {
       path: 'board',
       label: 'Board of Directors',
+      active: false,
     },
     {
       path: 'notes',
       label: 'Meeting Notes',
+      active: false,
     },
     {
       path: 'profile',
       label: 'Edit Profile',
+      active: false,
     },
   ];
 
@@ -58,9 +67,11 @@ export class SidebarComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.init();
-    const associationSubscription = this.userService.currentAssociationUpdated.subscribe(() => {
-      this.init();
-    });
+    const associationSubscription = this.userService.currentAssociationUpdated.subscribe(
+      () => {
+        this.init();
+      },
+    );
     const userSubscription = this.userService.userUpdated.subscribe(() => {
       this.init();
     });
@@ -69,7 +80,7 @@ export class SidebarComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     // unsubscribe to ensure no memory leaks
-    this.subscriptions.map(subscription => {
+    this.subscriptions.map((subscription) => {
       subscription.unsubscribe();
     });
   }
